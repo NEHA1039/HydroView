2e4dc3cc122e680a79e3449708b74882e6c1e463
NEHA1039 <nehathorat2004@gmail.com>
2025-11-01 19:59:37 +0530

    Day 2: Initial project setup (Java + React)

---
+++ hydroview-api/hydroview-api/src/main/java/com/hydroview/hydroview_api/HydroviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.hydroview.hydroview_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydroviewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HydroviewApiApplication.class, args);
+	}
+
+}
